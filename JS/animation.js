eced8c05cf81765304f907e8bf4b2208b7236bf6
riicard0 <riicardo.marttyns@gmail.com>
2021-04-02 23:13:34 -0300

    :package: Galeria

--- JS/animation.js
+++ JS/animation.js
@@ -8,3 +8,35 @@ $('.sessao-banner').slick({
     dots: true,
   });
 
+//GALERIA
+
+$('.sessao-galeria').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  centerMode: true,
+  centerPadding: '60px',
+  autoplay: true,
+  autoplaySpeed: 2000,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
+});
+
+//
